f98f2012507f0a39ffcd2f07921131dac804b268
Amirul Ishak <70368867+amirulishak093@users.noreply.github.com>
2023-09-19 12:45:50 +0800

    Change Google Sheet location

---
+++ +page.server.ts
@@ -0,0 +1,48 @@
+import type { Actions } from '@sveltejs/kit';
+import type { RequestEvent } from './$types';
+import { createRating } from '$lib/server/rating.service';
+
+export function load(event: RequestEvent) {
+	const site = event.params.site;
+	const location = event.params.location;
+
+	return { site, location };
+}
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+		const formData = await request.formData();
+
+		const score = parseInt(formData.get('score') as string);
+		const site = String(formData.get('site')) ?? '';
+		let location = String(formData.get('location')) ?? '';
+		const issues = String(formData.get('issues')) ?? '';
+
+		let issuesString = '';
+
+		issuesString = issues.split(' ').join('+');
+
+		if (location.includes('ad')) {
+			location = location.replace('ad', 'ta');
+		}
+
+		try {
+			const prefilledLink = `https://docs.google.com/forms/d/e/1FAIpQLSdKvFtTBo5LsiGnUcuru6XqnkbO_vbx4H5IJWH2nW-LeZuOZA/formResponse?usp=pp_url&entry.647138747=${site}&entry.353172185=${location}&entry.260921235=${score}&entry.642668930=${issuesString}`
+
+			await fetch(prefilledLink);
+			await createRating({ data: { score, site, location, issues } });
+
+			return {
+				success: true,
+				status: 'Form is submitted'
+			};
+		} catch (error) {
+			console.log(error);
+
+			return {
+				error,
+				score
+			};
+		}
+	}
+};
